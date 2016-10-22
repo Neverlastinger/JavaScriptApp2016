a2abc0f245e7c64c64b13c85059da478d6151a97
rvp <rvp@95appartement.net>
2016-10-22 20:34:41 +0300

    Added built resources

---
+++ dist/bundle.js
@@ -0,0 +1,12 @@
+var App = new function() {
+	this.init = function() {
+		console.log('App is initialized');
+	};
+};
+
+var init = function() {
+    App.init();
+};
+
+init();
+//# sourceMappingURL=bundle.js.map
